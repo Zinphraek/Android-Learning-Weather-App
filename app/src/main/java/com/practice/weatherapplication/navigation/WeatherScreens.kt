7cfb044394287f7f34de37650ce4585dc09dfbb9
zinphraek <zinphraek@gmail.com>
2024-08-08 00:44:41 -0400

    Set up navigation structure.

---
+++ app/src/main/java/com/practice/weatherapplication/navigation/WeatherScreens.kt
@@ -0,0 +1,10 @@
+package com.practice.weatherapplication.navigation
+
+enum class WeatherScreens {
+  FavoriteScreen,
+  AboutScreen,
+  MainScreen,
+  SearchScreen,
+  SettingsScreen,
+  SplashScreen,
+}
